75a57fc4eaedfddcd17555ecf48ae755467537af
Juanma Sanchez <juanmanuel.sanchezmateo@telefonica.com>
2020-10-22 16:09:49 +0200

    Snake sendGame added

--- snake_bot.py
+++ snake_bot.py
@@ -9,11 +9,22 @@ def start(update, context):
         text="Bot example text"
     )
 
+def snake(update, context):
+    context.bot.send_game(
+        chat_id=update.effective_chat.id,
+        game_short_name='snake'
+    )
+
 def main():
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
+
     start_handler = CommandHandler('start', start)
     dispatcher.add_handler(start_handler)
+
+    snake_handler = CommandHandler('snake', snake)
+    dispatcher.add_handler(snake_handler)
+
     updater.start_polling()
 
 main()
